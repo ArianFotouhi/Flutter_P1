293a93def65b364c47b56d610777e203efcb631e
unknown <fotouhiarian@gmail.com>
2023-09-21 16:33:15 -0400

    v1.1

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xff00ff89),
+        appBar: AppBar(
+          title: Text('AriApp'),
+          backgroundColor: Colors.blueGrey[700],
+        ),
+        body: SafeArea(
+          child: Container(
+            height: 400.0,
+            width: 400.0,
+            margin: EdgeInsets.symmetric(vertical: 30, horizontal: 8),
+            padding: EdgeInsets.fromLTRB(4, 4, 4, 4),
+            color: Colors.white, // Background color for the container
+            child: Column(
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      width: double.infinity,
+                      color: Color(0xffd8ffaa),
+                      child: Center(
+                        child: CircleAvatar(
+                          backgroundImage: AssetImage('images/profile.jpg'),
+                          radius: 50,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      child: Text('Evan',
+                          style: TextStyle(fontSize: 30, fontFamily: 'Roboto')),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  width: double.infinity,
+                  color: Color(0xffd8ffaa),
+                  child: Image(
+                    image: AssetImage('images/toronto.jpg'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
