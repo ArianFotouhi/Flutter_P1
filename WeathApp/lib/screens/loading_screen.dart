ffa15b983992eb862008b62794f47305af8dc043
unknown <fotouhiarian@gmail.com>
2023-10-04 18:26:46 -0400

    v2.0.4

--- WeathApp/lib/screens/loading_screen.dart
+++ WeathApp/lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import '../services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -14,17 +17,32 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocation() async {
-    try {
-      Position position = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.low);
-      print(position);
-    } catch (e) {
-      print(e);
-    }
+    Location location = Location();
+    await location.getCurrentLocation();
+  }
+
+  void getData() async {
+    double lat = 37;
+    double lon = -122;
+    String apiKey = 'd6230bcde1fd2213019455f7789e139e';
+    Uri apiUrl = Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$apiKey');
+
+    http.Response response = await http.get(apiUrl);
+    String data = response.body;
+    var temperature = jsonDecode(data)['main']['temp'];
+    var condition = jsonDecode(data)['weather'][0]['id'];
+    var cityName = jsonDecode(data)['name'];
+    print(data);
+    print(temperature);
+    print(condition);
+    print(cityName);
   }
 
   @override
   Widget build(BuildContext context) {
+    getData();
+
     return Scaffold(
       body: Center(
         child: ElevatedButton(
