0adde08c140c6ff433de4db2b62fd6d9c4c2c5db
unknown <fotouhiarian@gmail.com>
2023-10-06 11:21:07 -0400

    v2.1.5

--- ChatApp/lib/screens/welcome_screen.dart
+++ ChatApp/lib/screens/welcome_screen.dart
@@ -9,11 +9,53 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation animation;
+  late Animation animation2;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(duration: Duration(seconds: 2), vsync: this);
+
+    animation = CurvedAnimation(parent: controller, curve: Curves.bounceOut);
+    animation2 = ColorTween(begin: Color(0xffc2ecff), end: Color(0xff358dff))
+        .animate(controller);
+    controller.forward();
+
+    //bounce back
+    // controller.reverse(from: 1.0);
+
+    //bounce continuously
+
+    // animation.addStatusListener((status) {
+    //   if (status == AnimationStatus.completed) {
+    //     controller.reverse(from: 1.0);
+    //   } else if (status == AnimationStatus.dismissed) {
+    //     controller.forward();
+    //   }
+    //
+    // for continous, to stop it in case of leaving welcome screen
+    // @override
+    // void dispose(){
+    //   controller.dispose();
+    //   super.dispose();
+    // }
+    // });
+
+    controller.addListener(() {
+      setState(() {});
+      print(animation.value);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation2.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -26,13 +68,14 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                   tag: 'logo',
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    height: 60.0,
+                    height: controller.value * 60,
                   ),
                 ),
                 Text(
                   'Flash Chat',
                   style: TextStyle(
-                    fontSize: 45.0,
+                    color: Colors.white,
+                    fontSize: animation.value * 40,
                     fontWeight: FontWeight.w900,
                   ),
                 ),
