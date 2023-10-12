c1dce3e1b7f932fc8720e082e6eb513f50bfeb89
unknown <fotouhiarian@gmail.com>
2023-10-12 13:47:59 -0400

    v2.2.9

--- ToDoApp/lib/models/task_data.dart
+++ ToDoApp/lib/models/task_data.dart
@@ -1,21 +1,33 @@
-//import 'package:provider/provider.dart';
+import 'dart:collection';
 import 'package:flutter/foundation.dart';
 import '../models/task.dart';
 
 class TaskData extends ChangeNotifier{
 
-  List<Task> tasks = [
+  List<Task> _tasks = [
     Task(name: 'Buy Milk'),
     Task(name: 'Buy Orange'),
     Task(name: 'Buy Cheese'),
   ];
+
+UnmodifiableListView<Task> get tasks {
+  return UnmodifiableListView(_tasks);
+}
+
 int get taskCount {
-  return tasks.length;
+  return _tasks.length;
 }
 
 void addTask(String newTaskTitle){
   final task = Task(name: newTaskTitle);
-  tasks.add(task);
+  _tasks.add(task);
   notifyListeners();
 }
+
+void updateTask(Task task){
+  task.toggleDone();
+  notifyListeners();
+
+}
+
 }
